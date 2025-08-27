c151a5ebe3680c8469d8451e8c76eb192590c584
SamcraftSam <71342098+SamcraftSam@users.noreply.github.com>
2025-08-27 23:39:32 +0300

    reading data from file

---
+++ file_processor.py
@@ -0,0 +1,31 @@
+import numpy as np
+from scipy.io import wavfile
+
+class BasebandFileReader:
+    def __init__(self, filename, bits=16):
+        self.filename = filename
+        self.bits = bits
+        self.samplerate, self.data = wavfile.read(self.filename)
+        self.pos = 0
+        self.scale = 32767 if self.bits == 16 else (2**(self.bits - 1) - 1)
+        self.__stop_call = False
+
+    def receive_stream(self, callback, chunk_size=1024):
+        self.__stop_call = False
+        if self.data.dtype == np.float32 or self.data.dtype == np.float64:
+            self.data = (self.data * self.scale).astype(np.int16)
+
+        while self.pos < len(self.data) and not self.__stop_call:
+            chunk = self.data[self.pos:self.pos + chunk_size]
+            buf = chunk.astype(np.int16).tobytes()
+            callback(buf)
+            self.pos += chunk_size
+
+    def receive_samples(self, samplesnum):
+        if self.data.dtype == np.float32 or self.data.dtype == np.float64:
+            self.data = (self.data * self.scale).astype(np.int16)
+
+        return self.data[0:samplesnum]
+    
+    def stop(self):
+        self.__stop_call = True
--- receiver.py
+++ receiver.py
@@ -1,6 +1,8 @@
 import logging
 from abc import ABC, abstractmethod
 
+logger = logging.getLogger(__name__)
+
 try:
     from hackrf import HackRF
     hackrflib_found = True
@@ -13,16 +15,12 @@ try:
 except ImportError:
     rtlsdrlib_found = False
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
-
-
 class ReceiverBase(ABC):
     def receive(self, freq, bw, pipe=None, samples_num=0):
         self._set_freq(freq)
         self._set_sample_rate(bw)
         logger.info(f"Receiving: {freq / 1e6:.2f} MHz @ {bw / 1e6:.2f} MHz")
-        if samples_num > 0:
+        if samples_num is not None and samples_num > 0:
             return self._read_samples(samples_num)
         elif pipe is not None:
             self._start_stream(pipe)
@@ -78,6 +76,9 @@ class HackRFReceiver(ReceiverBase):
         self.sdr.stop_rx()
 
     def _set_gain(self, lna=16, vga=16, amp=False):
+        if lna > 40: logging.warning(f"Maximum LNA is 40, got {lna}")
+        if vga > 62: logging.warning(f"Maximum VGA is 62, got {vga}")
+
         self.sdr.lna_gain = min(max(0, lna), 40)
         self.sdr.vga_gain = min(max(0, vga), 62)
         self.sdr.amplifier_on = amp
@@ -136,19 +137,29 @@ class Receiver:
         if gain_args:
             self.device.set_gain(**gain_args)
 
-    def configure(self, freq=None, bw=None, samples_num=0, pipe=None):
+    def configure(self, freq=None, bw=None, samples_num=None, pipe=None):
         if freq is not None:
             self.config["freq"] = freq
         if bw is not None:
             self.config["bw"] = bw
-        self.config["samples_num"] = samples_num
-        self.config["pipe"] = pipe
+        if samples_num is not None:
+            self.config["samples_num"] = samples_num
+        if pipe is not None:
+            self.config["pipe"] = pipe
 
-    def receive(self):
+    def receive_stream(self):
         return self.device.receive(
             self.config["freq"],
             self.config["bw"],
             self.config["pipe"],
+            None
+        )
+    
+    def receive_samples(self):
+        return self.device.receive(
+            self.config["freq"],
+            self.config["bw"],
+            None,
             self.config["samples_num"]
         )
 
